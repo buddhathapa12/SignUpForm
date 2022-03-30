6b21ac24ded43f25a98df7f6fcec0ee3f5bb6639
unknown <buddhat645@gmail.com>
2022-03-31 00:11:39 +0545

    Removed the unused code from index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,1 @@
-// let vid = document.querySelector("#myVideo");
-
-// // vid.PlayBackRate = 2;
-// // window.addEventListener("DOMContentLoaded", setPlaySpeed());
-
-// const setPlaySpeed = () => {
-//   console.log("Hello World");
-//   vid.play = 2; //0.5;
-// };
-
 document.querySelector("#myVideo").playbackRate = 0.5;
