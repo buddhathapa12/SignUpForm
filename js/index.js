d2a7f793cf93d6b0217a7503948b66c4e0385861
unknown <buddhat645@gmail.com>
2022-03-31 00:00:05 +0545

    Changed the video playBackRate to 0.5

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// let vid = document.querySelector("#myVideo");
+
+// // vid.PlayBackRate = 2;
+// // window.addEventListener("DOMContentLoaded", setPlaySpeed());
+
+// const setPlaySpeed = () => {
+//   console.log("Hello World");
+//   vid.play = 2; //0.5;
+// };
+
+document.querySelector("#myVideo").playbackRate = 0.5;
