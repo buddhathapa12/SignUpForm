21a37d83bbb65bc41bfc6abe97b0249416d19c60
unknown <buddhat645@gmail.com>
2022-04-04 22:53:18 +0545

    Worked and completed the carousel section

--- js/carousel.js
+++ js/carousel.js
@@ -3,20 +3,35 @@ const next = document.querySelector(".next");
 
 const carouselElements = document.querySelector(".CarouselElements");
 
+const carouselSection = document.querySelector(".CarouselSection");
+
 let index = 0;
 
 let carouselWidth = document.querySelector(".carouselElements").offsetWidth;
 window.addEventListener("resize", () => {
-  index++;
+  index = 0;
+  console.log(carouselWidth);
+
+  console.log("screen" + window.innerWidth);
   carouselWidth = document.querySelector(".carouselElements").offsetWidth;
+  console.log("Hello" + carouselSection.offsetWidth);
 });
 
 next.addEventListener("click", () => {
-  carouselElements.style.transform = `translateX(-${carouselWidth}px)`;
+  if (window.innerWidth > 1160 && index < 1) index++;
+  if (window.innerWidth <= 1160 && window.innerWidth > 910 && index < 2)
+    index++;
+  if (window.innerWidth <= 910 && index < 4) index++;
+  console.log(index);
+  carouselElements.style.transform = `translateX(-${index * carouselWidth}px)`;
 });
 
 prev.addEventListener("click", () => {
-  carouselElements.style.transform = `translateX(-${0}px)`;
+  if (index > 0) {
+    index--;
+  }
+  next.style.display = "block";
+  carouselElements.style.transform = `translateX(-${index * carouselWidth}px)`;
 });
 
 window.addEventListener("resize", () => {
